a1053b20e2d54d182713906375f6174ee5a44990
Nabil <mokerpoker21@live.nl>
2023-05-16 00:28:49 +0200

    Initial commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ConsoleApp1
+﻿using System.Data.SQLite;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            string connectionString = "Data Source=/path/to/your/database.db";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                // Perform database operations here
+
+                connection.Close();
+            }
         }
     }
 }
